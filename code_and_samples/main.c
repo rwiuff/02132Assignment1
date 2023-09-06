cb16af1639d4906458aa50551f4016f85f4c8f08
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2023-09-06 15:46:05 +0200

    working on detection

--- code_and_samples/main.c
+++ code_and_samples/main.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "cbmp.h"
+#include <time.h>
 
 //Function to invert pixels of an image (negative)
 void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
@@ -22,6 +23,25 @@ void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsi
   }
 }
 
+int detect(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+  int detection = 0;
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGTH; y++)
+    {
+      for (int c = 0; c < BMP_CHANNELS; c++)
+      {
+        if(input_image[x][y][c] == 0){
+          detection += 1;
+        }
+      }
+    }
+  }
+  return detection;
+}
+
+
+
   //Declaring the array to store the image (unsigned char = unsigned 8 bit)
   unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
   unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
@@ -29,11 +49,13 @@ void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsi
 //Main function
 int main(int argc, char** argv)
 {
+  clock_t start, end;
+  double cpu_time_used;
   //argc counts how may arguments are passed
   //argv[0] is a string with the name of the program
   //argv[1] is the first command line argument (input image)
   //argv[2] is the second command line argument (output image)
-
+  start = clock();
   //Checking that 2 arguments are passed
   if (argc != 3)
   {
@@ -46,6 +68,10 @@ int main(int argc, char** argv)
   //Load image from file
   read_bitmap(argv[1], input_image);
 
+  //runs detection and prints number of detections 
+  int detectnumber = detect(input_image);
+  printf("Total number of detections: %d\n", detectnumber);
+
   //Run inversion
   invert(input_image,output_image);
 
@@ -53,5 +79,8 @@ int main(int argc, char** argv)
   write_bitmap(output_image, argv[2]);
 
   printf("Done!\n");
+  end = clock();
+  cpu_time_used = (end - start)*1000.0/CLOCKS_PER_SEC;
+  printf("Total time: %f ms\n", cpu_time_used);
   return 0;
 }
