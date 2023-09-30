ecad63f16be2666944351007363792519ee9ef46
Mikkel Andersen <miktotamirra@gmail.com>
2023-09-30 11:35:18 +0200

    Update functions.c

--- code_and_samples/functions.c
+++ code_and_samples/functions.c
@@ -44,11 +44,14 @@ void to_rgb(unsigned char tmp_image[BMP_WIDTH][BMP_HEIGTH], unsigned char output
 }
 
 // Erosion method
-void erode(unsigned char tmp_image[BMP_WIDTH][BMP_HEIGTH])
+unsigned char erode(unsigned char tmp_image[BMP_WIDTH][BMP_HEIGTH])
+
 {
-    unsigned char ErosionMap[BMP_WIDTH][BMP_HEIGTH] = {0};
-    for (int i = 1; i <= (BMP_WIDTH - 1); i++)
+	unsigned char Change=0;
+    unsigned char ErosionMap[BMP_WIDTH][BMP_HEIGTH] = {0}; // tmp_image sized matrix of 0's
+    for (int i = 1; i <= (BMP_WIDTH - 1); i++) 
     {
+		// Cut off Border to ensure erosion and fringe cases
         tmp_image[i - 1][0] = 0;
         tmp_image[i - 1][BMP_HEIGTH - 1] = 0;
 
@@ -56,7 +59,7 @@ void erode(unsigned char tmp_image[BMP_WIDTH][BMP_HEIGTH])
         tmp_image[BMP_WIDTH][i - 1] = 0;
         for (int j = 1; j <= (BMP_HEIGTH - 1); j++)
         {
-            
+            // Iterating over every pixel, if the sum of the surrounding area is not greater than 3x255 (i.e there's a black tile around) Then draw it up for erosion in the Erosion filter map 
             if ((tmp_image[i - 1][j] + tmp_image[i + 1][j] + tmp_image[i][j + 1] + tmp_image[i][j - 1] )<900)
             {
                 ErosionMap[i][j] = 0;
@@ -77,13 +80,16 @@ void erode(unsigned char tmp_image[BMP_WIDTH][BMP_HEIGTH])
             {
 				if (tmp_image[o][p] == 255){
 					
+					// Since it's cheaper to do all the erosions at once, we need to preserve the final pixels, so we check in a 2x2 area around if there's any connective pieces
+					
+					
 					if ((((tmp_image[o - 1][p] == 0 && tmp_image[o + 1][p] == 0) && (tmp_image[o][p + 1] == 0 && tmp_image[o][p - 1] == 0)) 
 					&& ((tmp_image[o - 1][p+1] == 0 && tmp_image[o + 1][p+1] == 0) && (tmp_image[o - 1][p-1] == 0 && tmp_image[o + 1][p-1] == 0)))&&
 					(((tmp_image[o - 2][p] == 0 && tmp_image[o + 2][p] == 0) && (tmp_image[o][p + 2] == 0 && tmp_image[o][p - 2] == 0)) 
 					&& ((tmp_image[o - 2][p+1] == 0 && tmp_image[o + 2][p+1] == 0) && (tmp_image[o - 2][p-1] == 0 && tmp_image[o + 2][p-1] == 0)))
 					&&
 					(((tmp_image[o - 2][p+2] == 0 && tmp_image[o + 2][p+2] == 0) && (tmp_image[o+1][p + 2] == 0 && tmp_image[o+1][p - 2] == 0)) 
-					&& ((tmp_image[o - 1][p+2] == 0 && tmp_image[o + 2][p-1] == 0) && (tmp_image[o - 2][p-2] == 0 && tmp_image[o - 1][p-2] == 0)))
+					&& ((tmp_image[o - 1][p+2] == 0 && tmp_image[o + 2][p-2] == 0) && (tmp_image[o - 2][p-2] == 0 && tmp_image[o - 1][p-2] == 0)))
 					
 					)
 					{
@@ -92,7 +98,9 @@ void erode(unsigned char tmp_image[BMP_WIDTH][BMP_HEIGTH])
 					
 					else
 					{
-
+					// This only gets triggered if There was a Pixel at any point up for Erosion, and it is fine to do so following the surrounding pixel criteria, 
+					//so we use it to check if there was a change and whether to stop erosion
+					  Change=1;
 					  tmp_image[o][p] = 0;
 					 }
 					
@@ -100,17 +108,7 @@ void erode(unsigned char tmp_image[BMP_WIDTH][BMP_HEIGTH])
 				}
             }
         }
-	
-     for (int o = 0; o < BMP_WIDTH; o++)
-    {
-        for (int p = 0; p < BMP_HEIGTH; p++)
-        {
-			if(tmp_image[o][p]==15){
-				tmp_image[o][p] = 0;
-			}
-	
-    }
-    }
+    return Change;
 }
 
 // Check for array overflow
